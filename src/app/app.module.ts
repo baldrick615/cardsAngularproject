9c850b9dae4f037a6c4cb09c49f5fb767d3b55ee
baldrick615 <tmcgrath28@gmail.com>
2023-04-29 11:13:52 -0500

    Completed styling and looping function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { NgOptimizedImage } from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

d32452058945b23d3775fa5d3ea3aab20dbbce19
baldrick615 <tmcgrath28@gmail.com>
2023-05-14 11:10:40 -0500

    Adding Pagination functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directives';
+  currentPage: number = 0;
+
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'In the Stream',
+      url: 'https://images.unsplash.com/photo-1683130461542-53fcbd04056f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'City Life',
+      url: 'https://images.unsplash.com/photo-1683219386001-738ce8d83f7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Boutique',
+      url: 'https://plus.unsplash.com/premium_photo-1664202526036-af7e7b7a02fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+  ]
+
+
 }
